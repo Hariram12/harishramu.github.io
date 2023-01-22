cf1ac32f0afb21aa072dd61e1ad506fed6265304
harishramur <72005717+harishramur@users.noreply.github.com>
2023-01-22 13:15:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,29 +31,29 @@ $(document).on('click', function(){
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get todays date and time
+    Get todays date and time
     var now = new Date().getTime();
     
-    // Find the distance between now and the count down date
+    Find the distance between now and the count down date
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
+    Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
+    Output the result in an element with id="demo"
     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
     
-    // If the count down is over, write some text 
+    If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
-// being a bit cool :p  
+being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
@@ -90,6 +90,6 @@ console.log('\n\n%c SAVE THE DATE: 1st Febraury, 2023!', styles);
 console.log('%cYour presence is requested!%c\n\nRegards: Harish ramu', styles1, styles2);
 
 console.log(
-    //`%cShaadi me zaroor aana!\n\n`,
+    `%cShaadi me zaroor aana!\n\n`,
     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
 )
